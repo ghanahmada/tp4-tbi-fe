172f44a03c3f6f04b6bd611d502d68e6c8ad83fd
Meefx <muhfakhri35@gmail.com>
2024-12-21 14:51:49 +0700

    feat: time metrics and dropdown

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -8,14 +8,19 @@ import SearchResult from "@/components/SearchResult.vue"
 const router = useRouter()
 const resultStore = useResultStore()
 const searchQuery = ref("")
-
-const search = async () => {
-  await resultStore.fetchResults(searchQuery.value)
-}
-
+const method = ref("BM25")
 const results = computed(() => resultStore.results)
 const loading = computed(() => resultStore.loading)
 const error = computed(() => resultStore.error)
+const retrievalTime = ref<number | null>(null)
+
+const search = async () => {
+  const startTime = performance.now()
+  await resultStore.fetchResults(searchQuery.value, method.value)
+  const endTime = performance.now()
+  retrievalTime.value = ((endTime - startTime) / 1000)
+}
+
 
 const viewDetail = (result: ResultInterface) => {
   resultStore.setSelectedResult(result)
@@ -36,12 +41,20 @@ const viewDetail = (result: ResultInterface) => {
   <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
     <!-- Search Bar -->
     <div class="flex justify-center mb-6 w-full md:w-1/2">
+      <select
+        v-model="method"
+        class="border rounded-l-lg p-2 focus:outline-none focus:ring focus:border-blue-300"
+      >
+        <option value="BM25">BM25</option>
+        <option value="ColBERT">ColBERT</option>
+        <option value="BERT">BERT</option>
+      </select>
       <input
         type="text"
         v-model="searchQuery"
         @keyup.enter="search"
         placeholder="Search..."
-        class="border rounded-l-lg p-2 w-full focus:outline-none focus:ring focus:border-blue-300"
+        class="border p-2 w-full focus:outline-none focus:ring focus:border-blue-300"
       />
       <button
         @click="search"
@@ -65,6 +78,9 @@ const viewDetail = (result: ResultInterface) => {
     <div v-if="results.length > 0 && !loading" class="mt-6">
       <h3 class="text-lg font-bold mb-4">
         Search Results for "{{ searchQuery }}"
+        <span v-if="retrievalTime !== null">
+          ({{ results.length }} results in {{ retrievalTime.toFixed(2) }}s)
+        </span>
       </h3>
       <SearchResult
         v-for="(result, index) in results"
