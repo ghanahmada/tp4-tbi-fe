9f4605da77ae68d722d127ae37ec3ad9d06ad822
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-12-23 01:30:02 +0700

    feat: routing to MainPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DetailPage from '../views/DetailPage.vue'
-import SearchPage from '../views/SearchPage.vue'
+import MainPage from '../views/MainPage.vue'
+import PaginatedResultPage from '../views/PaginatedResultPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "SearchPage", 
-      component: SearchPage
+      name: "MainPage",
+      component: MainPage
+    },
+    {
+      path: "/result",
+      name: "PaginatedResultPage",
+      component: PaginatedResultPage
     },
     {
       path: "/detail/:id",  // Add parameter in path
