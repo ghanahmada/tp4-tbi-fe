c11943fa15636663e444e83c081077f750417150
zuhdynadhif <zuhdy.nadhif.ayyasy@gmail.com>
2024-12-23 02:01:22 +0700

    feat: make it original

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -48,61 +48,38 @@ onMounted(() => {
 </style>
 
 <template>
-  <div class="min-h-screen flex flex-col items-center bg-gray-100">
-    <!-- Header -->
-    <header class="w-full h-[100px] bg-[#EAEAEA] flex items-center px-6 md:px-12 lg:px-20 fixed top-0 left-0 z-10">
-      <!-- Logo Fasilkom -->
-      <div class="flex items-center w-full md:w-[334px] h-[57px] flex-shrink-0 gap-4">
-        <!-- Logo Image -->
-        <img
-          src="https://i.ibb.co/4WF64VD/Fasilkom-UI-Logo-1.png"
-          alt="Fasilkom UI Logo"
-          class="w-[58px] h-[57px] object-cover"
-        />
-        <!-- Text Logo -->
-        <div class="w-full md:w-[263px] flex-shrink-0">
-          <!-- Title -->
-          <div class="text-[16px] md:text-[24px] font-bold text-black leading-none capitalize pb-1">
-            Information Retrieval
-          </div>
-          <!-- Subtitle -->
-          <div class="text-[10px] md:text-[16px] font-light text-black leading-none capitalize">
-            fasilkom UI
-          </div>
-        </div>
-      </div>
-      <!-- Search Bar -->
-      <div class="flex justify-center w-full md:w-3/5 pl-[10vh]">
-        <select
-          v-model="method"
-          class="border rounded-l-lg p-2 focus:outline-none focus:ring focus:border-blue-300"
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-4">
+    <!-- Search Bar -->
+    <div class="flex justify-center mb-6 w-full md:w-1/2">
+      <select
+        v-model="method"
+        class="border rounded-l-lg p-2 focus:outline-none focus:ring focus:border-blue-300"
+      >
+        <option
+          v-for="(item, idx) in methodList"
+          :key="idx"
+          :value="item"
         >
-          <option
-            v-for="(item, idx) in methodList"
-            :key="idx"
-            :value="item"
-          >
-            {{ item }}
-          </option>
-        </select>
-        <input
-          type="text"
-          v-model="searchQuery"
-          @keyup.enter="search"
-          placeholder="Search..."
-          class="border p-2 w-full focus:outline-none focus:ring focus:border-blue-300"
-        />
-        <button
-          @click="search"
-          class="bg-blue-500 text-white rounded-r-lg px-4 hover:bg-blue-600"
-        >
-          Search
-        </button>
-      </div> <!-- Close the missing div tag here -->
-    </header>
+          {{ item }}
+        </option>
+      </select>
+      <input
+        type="text"
+        v-model="searchQuery"
+        @keyup.enter="search"
+        placeholder="Search..."
+        class="border p-2 w-full focus:outline-none focus:ring focus:border-blue-300"
+      />
+      <button
+        @click="search"
+        class="bg-blue-500 text-white rounded-r-lg px-4 hover:bg-blue-600"
+      >
+        Search
+      </button>
+    </div>
 
     <!-- Loading State -->
-    <div v-if="loading" class="text-center mt-[140px]">
+    <div v-if="loading" class="text-center">
       <p>Loading...</p>
     </div>
 
@@ -112,7 +89,7 @@ onMounted(() => {
     </div>
 
     <!-- Search Results -->
-    <div v-if="results.length > 0 && !loading" class="mt-6 w-[63vw] mt-[140px]">
+    <div v-if="results.length > 0 && !loading" class="mt-6">
       <h3 class="text-lg font-bold mb-4">
         Search Results for "{{ searchQuery }}"
         <span v-if="retrievalTime !== null">
@@ -126,17 +103,5 @@ onMounted(() => {
         @view-detail="viewDetail"
       />
     </div>
-
-    <footer class="w-full h-[80px] bg-[#EAEAEA] flex items-center px-6 md:px-12 lg:px-20 fixed bottom-0 left-0 z-10">
-      <div class="w-full flex-shrink-0">
-          <!-- Title -->
-          <div class="text-[16px] md:text-[16px] font-LIGHT text-black leading-none capitalize pb-1">
-            Result are not personalized
-          </div>
-          <div class="text-[10px] md:text-[12px] font-LIGHT text-black leading-none capitalize pb-1">
-            © 2024 Information Retrieval - Faculty of Computer Science
-          </div>
-        </div>
-    </footer>
   </div>
 </template>
